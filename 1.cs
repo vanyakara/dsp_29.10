35ce5c9c23fa9dfdcb5c4f98603d021342ac81f8
vanyakara <147335690+vanyakara@users.noreply.github.com>
2024-10-29 09:42:23 +0200

    Add files via upload

---
+++ 1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dsp_29._10
+{
+    public abstract class Juice
+    {
+       public abstract void Info();
+    }
+    public class AppleJuice : Juice
+    {
+        public override void Info()
+        {
+            Console.WriteLine("Сок от ябълка. Съдържание на плодове: 70%");
+        }
+    }
+    public class CherryJuice : Juice
+    {
+        public override void Info()
+        {
+            Console.WriteLine("Сок от вишна. Съдържание на плодове: 60%");
+        }
+    }
+    public class OrangeJuice : Juice
+    {
+        public override void Info()
+        {
+            Console.WriteLine("Сок от портокал. Съдържание на плодове: 80%");
+        }
+    }
+    public abstract class JuiceFactory
+    {
+        public abstract Juice CreateJuice();
+    }
+    public class AppleJuiceFactory : JuiceFactory
+    {
+        public override Juice CreateJuice()
+        {
+            return new AppleJuice();
+        }
+    }
+    public class CherryJuiceFactory : JuiceFactory
+    {
+        public override Juice CreateJuice()
+        {
+            return new CherryJuice();
+        }
+    }
+    public class OrangeJuiceFactory : JuiceFactory
+    {
+        public override Juice CreateJuice()
+        {
+            return new OrangeJuice();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JuiceFactory appleFactory = new AppleJuiceFactory();
+            Juice appleJuice = appleFactory.CreateJuice();
+            appleJuice.Info();
+
+            JuiceFactory cherryFactory = new CherryJuiceFactory();
+            Juice cherryJuice = cherryFactory.CreateJuice();
+            cherryJuice.Info();
+
+            JuiceFactory orangeFactory = new OrangeJuiceFactory();
+            Juice orangeJuice = orangeFactory.CreateJuice();
+            orangeJuice.Info();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 2.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Point
+    {
+        public double X { get; private set; }
+        public double Y { get; private set; }
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+        public void InputPoint()
+        {
+            Console.Write("Въведете X: ");
+            X = double.Parse(Console.ReadLine());
+            Console.Write("Въведете Y: ");
+            Y = double.Parse(Console.ReadLine());
+        }
+        public void PrintPoint()
+        {
+            Console.WriteLine($"Точка: ({X}, {Y})");
+        }
+    }
+    public class Area
+    {
+        public static Area instance;
+        public double MinX { get; }
+        public double MaxX { get; }
+        public double MinY { get; }
+        public double MaxY { get; }
+        public Area(double minX,double maxX,double minY,double maxY)
+        {
+            MinX = minX;
+            MaxX = maxX;
+            MinY = minY;
+            MaxY = maxY;
+        }
+        public static Area GetInstance(double minX,double maxX, double minY, double maxY)
+        {
+            if (instance == null)
+            {
+                instance = new Area(minX, maxX, minY, maxY);
+            }
+            return instance;
+        }
+        public bool IsPointInArea(Point point)
+        {
+            return point.X >= MinX && point.X <= MaxX && point.Y >= MinY && point.Y <= MaxY;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double minX = 0;
+            double maxX = 10;
+            double minY = 0;
+            double maxY = 10;
+            Area area = Area.GetInstance(minX, maxX, minY, maxY);
+            List<Point> points = new List<Point>();
+            Console.Write("Въведете броя точки, които искате да добавите: ");
+            int numberOfPoints = int.Parse(Console.ReadLine());
+            for(int i = 0; i < numberOfPoints; i++)
+            {
+                Console.WriteLine($"Въвеждане на точка {i + 1}: ");
+                Point point = new Point(0, 0);
+                point.InputPoint();
+                if (area.IsPointInArea(point))
+                {
+                    points.Add(point);
+                    Console.WriteLine("Точката е добавена.");
+                }
+                else
+                {
+                    Console.WriteLine("Точката не попада в зададената област.");
+                }
+            }
+            Console.WriteLine("\nВалидни точки в областта: ");
+            foreach (var point in points)
+            {
+                point.PrintPoint();
+            }
+            Console.ReadLine();
+        }
+    }
+}
